59d8e84a3c1db14e8c1385d904ab80779df922e1
Roger_Zuriaga <rogerdroid1700@gmail.com>
2026-02-02 15:05:05 -0600

    fix en el padding de la screen crear evento

--- lib/features/events/presentation/screens/create_event_screen.dart
+++ lib/features/events/presentation/screens/create_event_screen.dart
@@ -108,7 +108,7 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
       body: Form(
         key: _formKey,
         child: ListView(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 100),
           children: [
             // Título
             TextFormField(
@@ -272,7 +272,6 @@ class _CreateEventScreenState extends ConsumerState<CreateEventScreen> {
                     : const Text('Crear Evento'),
               ),
             ),
-            const SizedBox(height: 16),
           ],
         ),
       ),
